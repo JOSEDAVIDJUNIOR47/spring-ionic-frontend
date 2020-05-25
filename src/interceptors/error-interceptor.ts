8df835269fa1b10b5ef35b59fe17d9fb81fa4c52
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2020-05-24 23:18:17 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -30,6 +31,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                      case 403:
                          this.handle403();
                          break;
+                    case 422:
+                          this.handle422(errorObj);
+                          break;   
                       default:
                           this.handleDefaultError(errorObj);   
                  }
@@ -48,9 +52,24 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
         alert.present();
     }
+
     handle403(){
         this.storage.setLocalUser(null);
     }
+
+    handle422(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'OK'}
+            ]
+        });
+        alert.present();
+    }
+
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create({
             title: 'Erro '+errorObj.status+': ' + errorObj.error,
@@ -63,8 +82,16 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
 
     }      
-}
 
+
+    private listErrors(messages : FieldMessage[]) : string{
+    let s : string = '';
+    for(var i=0; i< messages.length; i++){
+        s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+    }
+    return s;
+    }
+}
 export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
     useClass: ErrorInterceptor,
