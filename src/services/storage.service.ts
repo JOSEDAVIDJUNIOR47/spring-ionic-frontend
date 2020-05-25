5840133bd24f1ebdfce3833dc52859a631c7daee
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2020-05-25 18:20:06 -0300

    Criando pagina de carrinho de compras

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { LocalUser } from "../models/local_user";
 import { STORAGE_KEYS } from "../config/store_keys.config";
 import { jsonpCallbackContext } from "@angular/common/http/src/module";
+import { Cart } from "../models/cart";
 
 export class StorageService{
 
@@ -20,6 +21,26 @@ export class StorageService{
         }
         else {
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+        }
+
+    }
+
+    getCart() : Cart {
+        let str = localStorage.getItem(STORAGE_KEYS.cart);
+        if(str == null){
+            return null;
+        }
+        else {
+            return JSON.parse(str);
+        }
+    }
+
+    setCart(obj : Cart){
+        if(obj == null){
+            localStorage.removeItem(STORAGE_KEYS.cart);
+        }
+        else {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
         }
 
     }
